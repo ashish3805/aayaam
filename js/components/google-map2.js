db0796d66ac6350e32488312627762f4761c2892
srajan96 <srajan1996@gmail.com>
2017-02-27 22:13:46 +0530

    added maps and contact form

--- js/components/google-map2.js
+++ js/components/google-map2.js
@@ -2,9 +2,9 @@
 
 jQuery(document).ready(function($){
   //set your google maps parameters
-  var latitude = 41.850,
-      longitude = -73.961,
-    map_zoom = 6;
+  var latitude = 22.7252,
+      longitude = 75.8713,
+    map_zoom = 16;
 
   //google map custom marker icon - .png fallback for IE11
   var is_internetExplorer11= navigator.userAgent.toLowerCase().indexOf('trident') > -1;
@@ -16,175 +16,176 @@ jQuery(document).ready(function($){
     brightness_value= 40;
 
   //we define here the style of the map
-  var style= [ 
-    {
-      //set saturation for the labels on the map
-      elementType: 'labels',
-      stylers: [
-        {saturation: saturation_value}
-      ]
-    },  
-      { //poi stands for point of interest - don't show these lables on the map 
-      featureType: 'poi',
-      elementType: 'labels',
-      stylers: [
-        {visibility: 'off'}
-      ]
-    },
-    {
-      //don't show highways lables on the map
-          featureType: 'road.highway',
-          elementType: 'labels',
-          stylers: [
-              {visibility: 'off'}
-          ]
-      }, 
-    {   
-      //don't show local road lables on the map
-      featureType: 'road.local', 
-      elementType: 'labels.icon', 
-      stylers: [
-        {visibility: 'off'} 
-      ] 
-    },
-    { 
-      //don't show arterial road lables on the map
-      featureType: 'road.arterial', 
-      elementType: 'labels.icon', 
-      stylers: [
-        {visibility: 'off'}
-      ] 
-    },
-    {
-      //don't show road lables on the map
-      featureType: 'road',
-      elementType: 'geometry.stroke',
-      stylers: [
-        {visibility: 'off'}
-      ]
-    }, 
-    //style different elements on the map
-    { 
-      featureType: 'transit', 
-      elementType: 'geometry.fill', 
-      stylers: [
-        { hue: main_color },
-        { visibility: 'on' }, 
-        { lightness: brightness_value }, 
-        { saturation: saturation_value }
-      ]
-    }, 
-    {
-      featureType: 'poi',
-      elementType: 'geometry.fill',
-      stylers: [
-        { hue: main_color },
-        { visibility: 'on' }, 
-        { lightness: brightness_value }, 
-        { saturation: saturation_value }
-      ]
-    },
-    {
-      featureType: 'poi.government',
-      elementType: 'geometry.fill',
-      stylers: [
-        { hue: main_color },
-        { visibility: 'on' }, 
-        { lightness: brightness_value }, 
-        { saturation: saturation_value }
-      ]
-    },
-    {
-      featureType: 'poi.sport_complex',
-      elementType: 'geometry.fill',
-      stylers: [
-        { hue: main_color },
-        { visibility: 'on' }, 
-        { lightness: brightness_value }, 
-        { saturation: saturation_value }
-      ]
-    },
-    {
-      featureType: 'poi.attraction',
-      elementType: 'geometry.fill',
-      stylers: [
-        { hue: main_color },
-        { visibility: 'on' }, 
-        { lightness: brightness_value }, 
-        { saturation: saturation_value }
-      ]
-    },
-    {
-      featureType: 'poi.business',
-      elementType: 'geometry.fill',
-      stylers: [
-        { hue: main_color },
-        { visibility: 'on' }, 
-        { lightness: brightness_value }, 
-        { saturation: saturation_value }
-      ]
-    },
-    {
-      featureType: 'transit',
-      elementType: 'geometry.fill',
-      stylers: [
-        { hue: main_color },
-        { visibility: 'on' }, 
-        { lightness: brightness_value }, 
-        { saturation: saturation_value }
-      ]
-    },
-    {
-      featureType: 'transit.station',
-      elementType: 'geometry.fill',
-      stylers: [
-        { hue: main_color },
-        { visibility: 'on' }, 
-        { lightness: brightness_value }, 
-        { saturation: saturation_value }
-      ]
-    },
-    {
-      featureType: 'landscape',
-      stylers: [
-        { hue: main_color },
-        { visibility: 'on' }, 
-        { lightness: brightness_value }, 
-        { saturation: saturation_value }
-      ]
-      
-    },
-    {
-      featureType: 'road',
-      elementType: 'geometry.fill',
-      stylers: [
-        { hue: main_color },
-        { visibility: 'on' }, 
-        { lightness: brightness_value }, 
-        { saturation: saturation_value }
-      ]
-    },
-    {
-      featureType: 'road.highway',
-      elementType: 'geometry.fill',
-      stylers: [
-        { hue: main_color },
-        { visibility: 'on' }, 
-        { lightness: brightness_value }, 
-        { saturation: saturation_value }
-      ]
-    }, 
-    {
-      featureType: 'water',
-      elementType: 'geometry',
-      stylers: [
-        { hue: main_color },
-        { visibility: 'on' }, 
-        { lightness: brightness_value }, 
-        { saturation: saturation_value }
-      ]
-    }
-  ];
+  var style= [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#242f3e"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#746855"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#242f3e"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.country",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "on"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.locality",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#d59563"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#d59563"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#263c3f"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#6b9a76"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#38414e"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#212a37"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9ca5b3"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#746855"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#1f2835"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#f3d19c"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#2f3948"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#d59563"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#17263c"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#515c6d"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#17263c"
+      }
+    ]
+  }
+]
     
   //set google map options
   var map_options = {
@@ -205,9 +206,9 @@ jQuery(document).ready(function($){
   var contentString = '<div id="content">'+
     '<div id="siteNotice">'+
     '</div>'+
-    '<h2 id="firstHeading" class="firstHeading">Brooklyn</h2>'+
+    '<h2 id="firstHeading" class="firstHeading">SGSITS</h2>'+
     '<div id="bodyContent">'+
-    '<p>277 Bedford Avenue, <br> Brooklyn, NY 11211, <br> New York, USA</p>'+
+    '<p>23 Park Road, <br> Indore <br> Madhya Pradesh</p>'+
     '</div>'+
     '</div>';
 
