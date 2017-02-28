1538e2073d7aab437e77c64533e4c02235e89edc
srajan96 <srajan1996@gmail.com>
2017-02-28 16:36:31 +0530

    adding events page

--- events/js/main.js
+++ events/js/main.js
@@ -1,211 +1,76 @@
-/**
- * main.js
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2015, Codrops
- * http://www.codrops.com
- */
-(function() {
+jQuery(document).ready(function($){
+	var gallery = $('.cd-gallery'),
+		foldingPanel = $('.cd-folding-panel'),
+		mainContent = $('.cd-main');
+	/* open folding content */
+	gallery.on('click', 'a', function(event){
+		event.preventDefault();
+		openItemInfo($(this).attr('href'));
+	});
 
-	var bodyEl = document.body,
-		docElem = window.document.documentElement,
-		support = { transitions: Modernizr.csstransitions },
-		// transition end event name
-		transEndEventNames = { 'WebkitTransition': 'webkitTransitionEnd', 'MozTransition': 'transitionend', 'OTransition': 'oTransitionEnd', 'msTransition': 'MSTransitionEnd', 'transition': 'transitionend' },
-		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-		onEndTransition = function( el, callback ) {
-			var onEndCallbackFn = function( ev ) {
-				if( support.transitions ) {
-					if( ev.target != this ) return;
-					this.removeEventListener( transEndEventName, onEndCallbackFn );
-				}
-				if( callback && typeof callback === 'function' ) { callback.call(this); }
-			};
-			if( support.transitions ) {
-				el.addEventListener( transEndEventName, onEndCallbackFn );
-			}
-			else {
-				onEndCallbackFn();
-			}
-		},
-		gridEl = document.getElementById('theGrid'),
-		sidebarEl = document.getElementById('theSidebar'),
-		gridItemsContainer = gridEl.querySelector('section.grid'),
-		contentItemsContainer = gridEl.querySelector('section.content'),
-		gridItems = gridItemsContainer.querySelectorAll('.grid__item'),
-		contentItems = contentItemsContainer.querySelectorAll('.content__item'),
-		closeCtrl = contentItemsContainer.querySelector('.close-button'),
-		current = -1,
-		lockScroll = false, xscroll, yscroll,
-		isAnimating = false,
-		menuCtrl = document.getElementById('menu-toggle'),
-		menuCloseCtrl = sidebarEl.querySelector('.close-button');
+	/* close folding content */
+	foldingPanel.on('click', '.cd-close', function(event){
+		event.preventDefault();
+		toggleContent('', false);
+	});
+	gallery.on('click', function(event){
+		/* detect click on .cd-gallery::before when the .cd-folding-panel is open */
+		if($(event.target).is('.cd-gallery') && $('.fold-is-open').length > 0 ) toggleContent('', false);
+	})
 
-	/**
-	 * gets the viewport width and height
-	 * based on http://responsejs.com/labs/dimensions/
-	 */
-	function getViewport( axis ) {
-		var client, inner;
-		if( axis === 'x' ) {
-			client = docElem['clientWidth'];
-			inner = window['innerWidth'];
+	function openItemInfo(url) {
+		var mq = viewportSize();
+		if( gallery.offset().top > $(window).scrollTop() && mq != 'mobile') {
+			/* if content is visible above the .cd-gallery - scroll before opening the folding panel */
+			$('body,html').animate({
+				'scrollTop': gallery.offset().top
+			}, 100, function(){ 
+	           	toggleContent(url, true);
+	        }); 
+	    } else if( gallery.offset().top + gallery.height() < $(window).scrollTop() + $(window).height()  && mq != 'mobile' ) {
+			/* if content is visible below the .cd-gallery - scroll before opening the folding panel */
+			$('body,html').animate({
+				'scrollTop': gallery.offset().top + gallery.height() - $(window).height()
+			}, 100, function(){ 
+	           	toggleContent(url, true);
+	        });
+		} else {
+			toggleContent(url, true);
 		}
-		else if( axis === 'y' ) {
-			client = docElem['clientHeight'];
-			inner = window['innerHeight'];
-		}
-		
-		return client < inner ? inner : client;
-	}
-	function scrollX() { return window.pageXOffset || docElem.scrollLeft; }
-	function scrollY() { return window.pageYOffset || docElem.scrollTop; }
-
-	function init() {
-		initEvents();
 	}
 
-	function initEvents() {
-		[].slice.call(gridItems).forEach(function(item, pos) {
-			// grid item click event
-			item.addEventListener('click', function(ev) {
-				ev.preventDefault();
-				if(isAnimating || current === pos) {
-					return false;
-				}
-				isAnimating = true;
-				// index of current item
-				current = pos;
-				// simulate loading time..
-				classie.add(item, 'grid__item--loading');
-				setTimeout(function() {
-					classie.add(item, 'grid__item--animate');
-					// reveal/load content after the last element animates out (todo: wait for the last transition to finish)
-					setTimeout(function() { loadContent(item); }, 500);
-				}, 1000);
+	function toggleContent(url, bool) {
+		if( bool ) {
+			/* load and show new content */
+			var foldingContent = foldingPanel.find('.cd-fold-content');
+			foldingContent.load(url+' .cd-fold-content > *', function(event){
+				setTimeout(function(){
+					$('body').addClass('overflow-hidden');
+					foldingPanel.addClass('is-open');
+					mainContent.addClass('fold-is-open');
+				}, 100);
+				
 			});
-		});
-
-		closeCtrl.addEventListener('click', function() {
-			// hide content
-			hideContent();
-		});
-
-		// keyboard esc - hide content
-		document.addEventListener('keydown', function(ev) {
-			if(!isAnimating && current !== -1) {
-				var keyCode = ev.keyCode || ev.which;
-				if( keyCode === 27 ) {
-					ev.preventDefault();
-					if ("activeElement" in document)
-    					document.activeElement.blur();
-					hideContent();
-				}
-			}
-		} );
-
-		// hamburger menu button (mobile) and close cross
-		menuCtrl.addEventListener('click', function() {
-			if( !classie.has(sidebarEl, 'sidebar--open') ) {
-				classie.add(sidebarEl, 'sidebar--open');	
-			}
-		});
-
-		menuCloseCtrl.addEventListener('click', function() {
-			if( classie.has(sidebarEl, 'sidebar--open') ) {
-				classie.remove(sidebarEl, 'sidebar--open');
-			}
-		});
-	}
-
-	function loadContent(item) {
-		// add expanding element/placeholder 
-		var dummy = document.createElement('div');
-		dummy.className = 'placeholder';
-
-		// set the width/heigth and position
-		dummy.style.WebkitTransform = 'translate3d(' + (item.offsetLeft - 5) + 'px, ' + (item.offsetTop - 5) + 'px, 0px) scale3d(' + item.offsetWidth/gridItemsContainer.offsetWidth + ',' + item.offsetHeight/getViewport('y') + ',1)';
-		dummy.style.transform = 'translate3d(' + (item.offsetLeft - 5) + 'px, ' + (item.offsetTop - 5) + 'px, 0px) scale3d(' + item.offsetWidth/gridItemsContainer.offsetWidth + ',' + item.offsetHeight/getViewport('y') + ',1)';
-
-		// add transition class 
-		classie.add(dummy, 'placeholder--trans-in');
-
-		// insert it after all the grid items
-		gridItemsContainer.appendChild(dummy);
-		
-		// body overlay
-		classie.add(bodyEl, 'view-single');
-
-		setTimeout(function() {
-			// expands the placeholder
-			dummy.style.WebkitTransform = 'translate3d(-5px, ' + (scrollY() - 5) + 'px, 0px)';
-			dummy.style.transform = 'translate3d(-5px, ' + (scrollY() - 5) + 'px, 0px)';
-			// disallow scroll
-			window.addEventListener('scroll', noscroll);
-		}, 25);
-
-		onEndTransition(dummy, function() {
-			// add transition class 
-			classie.remove(dummy, 'placeholder--trans-in');
-			classie.add(dummy, 'placeholder--trans-out');
-			// position the content container
-			contentItemsContainer.style.top = scrollY() + 'px';
-			// show the main content container
-			classie.add(contentItemsContainer, 'content--show');
-			// show content item:
-			classie.add(contentItems[current], 'content__item--show');
-			// show close control
-			classie.add(closeCtrl, 'close-button--show');
-			// sets overflow hidden to the body and allows the switch to the content scroll
-			classie.addClass(bodyEl, 'noscroll');
-
-			isAnimating = false;
-		});
-	}
-
-	function hideContent() {
-		var gridItem = gridItems[current], contentItem = contentItems[current];
-
-		classie.remove(contentItem, 'content__item--show');
-		classie.remove(contentItemsContainer, 'content--show');
-		classie.remove(closeCtrl, 'close-button--show');
-		classie.remove(bodyEl, 'view-single');
-
-		setTimeout(function() {
-			var dummy = gridItemsContainer.querySelector('.placeholder');
-
-			classie.removeClass(bodyEl, 'noscroll');
-
-			dummy.style.WebkitTransform = 'translate3d(' + gridItem.offsetLeft + 'px, ' + gridItem.offsetTop + 'px, 0px) scale3d(' + gridItem.offsetWidth/gridItemsContainer.offsetWidth + ',' + gridItem.offsetHeight/getViewport('y') + ',1)';
-			dummy.style.transform = 'translate3d(' + gridItem.offsetLeft + 'px, ' + gridItem.offsetTop + 'px, 0px) scale3d(' + gridItem.offsetWidth/gridItemsContainer.offsetWidth + ',' + gridItem.offsetHeight/getViewport('y') + ',1)';
-
-			onEndTransition(dummy, function() {
-				// reset content scroll..
-				contentItem.parentNode.scrollTop = 0;
-				gridItemsContainer.removeChild(dummy);
-				classie.remove(gridItem, 'grid__item--loading');
-				classie.remove(gridItem, 'grid__item--animate');
-				lockScroll = false;
-				window.removeEventListener( 'scroll', noscroll );
-			});
+		} else {
+			/* close the folding panel */
+			var mq = viewportSize();
+			foldingPanel.removeClass('is-open');
+			mainContent.removeClass('fold-is-open');
 			
-			// reset current
-			current = -1;
-		}, 25);
-	}
-
-	function noscroll() {
-		if(!lockScroll) {
-			lockScroll = true;
-			xscroll = scrollX();
-			yscroll = scrollY();
+			(mq == 'mobile' || $('.no-csstransitions').length > 0 ) 
+				/* according to the mq, immediately remove the .overflow-hidden or wait for the end of the animation */
+				? $('body').removeClass('overflow-hidden')
+				
+				: mainContent.find('.cd-item').eq(0).one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+					$('body').removeClass('overflow-hidden');
+					mainContent.find('.cd-item').eq(0).off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+				});
 		}
-		window.scrollTo(xscroll, yscroll);
+		
 	}
 
-	init();
-
-})();
+	function viewportSize() {
+		/* retrieve the content value of .cd-main::before to check the actua mq */
+		return window.getComputedStyle(document.querySelector('.cd-main'), '::before').getPropertyValue('content').replace(/"/g, "").replace(/'/g, "");
+	}
+});
