1ef92992f4fa8fea12defecb07c982d731a720e7
Aditya Vashishtha <vashishthaaditya20@gmail.com>
2017-03-01 23:58:43 +0530

    Updated Python script, and added item pages in it

--- events/contentGen.py
+++ events/contentGen.py
@@ -13,24 +13,23 @@ for x in data:
         for gg in tags:
             ttg = "".join((gg,'|',ttg))
         
-        head = '<!doctype html><html lang="en" class="no-js"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="css/reset.css"><link rel="stylesheet" href="css/style.css"><script src="js/modernizr.js"></script><title>Robo War</title><link href="event-details/min/plugin-min.css" type="text/css" rel="stylesheet"><link href="event-details/min/custom-min.css" type="text/css" rel="stylesheet"></head><body><div class="cd-fold-content single-page" style="padding: 0;"><div class="banner section no-pad-bot">'
+        head = ''.join(('<!doctype html><html lang="en" class="no-js"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="css/reset.css"><script src="js/modernizr.js"></script><link rel="stylesheet" href="css/style_new.css"><link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> <title>',ename,'</title><link href="event-details/min/plugin-min.css" type="text/css" rel="stylesheet"><link href="event-details/min/custom-min.css" type="text/css" rel="stylesheet"></head><body><div class="body-single cd-fold-content single-page" style="padding: 0;"><div class="banner section no-pad-bot">'))
         #isrc = data[x][i]["posterUrl"]
-        src = data[x][i]["posterUrl"]
-        sec = ''.join(('<h1>',ename,'</h1><img src="',src,'" />        <div class="filter"></div>        </div>        <div id="intro" class="section scrollspy">            <div class="container">       <h4 style="text-align: center; font-weight: 800;font-weight: 1.5em;">',ttg,'',dept,'</h4><br><br>         <div class="row">                    <div class="col s12 m4 l4">                        <div class="center promo promo-example">                            <i class="material-icons">date_range</i>                            <br>'))
+        src = data[x][i]["posterHostedUrl"]
+        sec = ''.join(('<h1>',ename,'</h1><img src="',src,'" />        <div class="filter"></div>        </div>        <div id="intro" class="section scrollspy">            <div class="container">       <h4 class="promo-caption" style="text-align: center; font-weight: 800;font-weight: 1em;">',ttg,'',dept,'</h4><br><br><div class="row"><div class="col s12 m4 l4"><div class="center promo promo-example">                            <i class="material-icons">date_range</i>'))
         head = "".join((head,sec))
         
         venue = "Cafeteria"
-        time = 'March 10 <br>Time 11:00 AM to 12:00 PM'
-
-        time = "".join(('<p class="light center">',time,'</p>                        </div>                    </div>'))
+        time = '<br><h5 class="promo-caption">10 March</h5> <p class="light center">Time 11:00 AM to 12:00 PM</p> </div></div>'
+        #time = "".join((time,''))
         head = "".join((head,time))        
         contact = str(data[x][i]["contactMobile"])
         email = data[x][i]["email"]
         name = data[x][i]["nameOfContactPerson"]
         #link = data[x][i]["websiteOfTheEvent"]        
         desc = data[x][i]["description"]
-        reg = data[x][i]["onlineRegistrationDetails"]
-        t =  "".join(('<div class="col s12 m4 l4">                        <div class="center promo promo-example">                            <i class="material-icons">location_on</i>                            <br>                            <h5 class="promo-caption">Venue</h5>                           <p class="light center">',venue,'</p>                        </div>                    </div>                    <div class="col s12 m4 l4">                        <div class="center promo promo-example">                            <i class="material-icons">call</i>                           <br>                            <h5 class="promo-caption">',name,'</h5>                            <p class="light center">',contact,'<br>',email,'</p>                        </div>                    </div>                    <div class="col s12">                        <h2 class="center eader text_h2"> ',desc,'</h2>                    </div>                </div>            </div>        </div>    </div></body><script src="js/jquery-2.1.1.js"></script><script src="js/main.js"></script><script src="event-details/min/plugin-min.js"></script><script src="event-details/min/custom-min.js"></script><!-- Resource jQuery --></body></html>'))  
+        #reg = data[x][i]["onlineRegistrationDetails"]
+        t =  "".join(('<div class="col s12 m4 l4"><div class="center promo promo-example"><i class="material-icons">location_on</i><br><h5 class="promo-caption">Venue</h5>                           <p class="light center">',venue,'</p>                        </div>                    </div>                    <div class="col s12 m4 l4">                        <div class="center promo promo-example">                            <i class="material-icons">call</i>                           <br><h5 class="promo-caption">',name,'</h5>                            <p class="light center">',contact,'<br>',email,'</p>                        </div>                    </div>                    <div class="col s12">                        <h2 class="center header text_h2"> ',desc,'</h2>                    </div>                </div>            </div>        </div>    </div></body><script src="js/jquery-2.1.1.js"></script><script src="js/main.js"></script><script src="event-details/min/plugin-min.js"></script><script src="event-details/min/custom-min.js"></script><!-- Resource jQuery --></body></html>'))  
         filename = ''.join(('item-',str(i+1),'.html'))
         head = "".join((head,t))
         obj = open(filename, 'wb')
