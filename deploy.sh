98bf5e4aad74612c9ed77abdda4b998fb79f75ce
menkudle <shubham.menkudle.cse@gmail.com>
2017-03-02 22:04:09 +0530

    automated

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
  
 ssh ubuntu@aayaam.sgsits.ac.in <<EOF
   cd /var/www/aayaam.sgsits.ac.in/public_html
   sudo git pull
+  sudo python /var/www/aayaam.sgsits.ac.in/public_html/events/sheetFetch/quickstart.py
   exit
 EOF
