3c8755d5e1d9f1087ddd6f03449a54f9827a20cb
menkudle <shubham.menkudle.cse@gmail.com>
2017-03-02 22:27:55 +0530

    issue resolved , deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 ssh ubuntu@aayaam.sgsits.ac.in <<EOF
   cd /var/www/aayaam.sgsits.ac.in/public_html
   sudo git pull
+  cd /var/www/aayaam.sgsits.ac.in/public_html/public_html/events/sheetFetch/
   sudo python /var/www/aayaam.sgsits.ac.in/public_html/events/sheetFetch/quickstart.py
   exit
 EOF
