ab8e29cd83956b39c581eff161a1cca9b1023f13
srajan96 <srajan1996@gmail.com>
2017-03-05 00:10:55 +0530

    added recaptcha

--- mail.php
+++ mail.php
@@ -7,17 +7,25 @@ $message=$_POST['message'];
 date_default_timezone_set('Asia/Kolkata');
 $time = date('m/d/Y h:i:s a', time());
 
-$to = 'aayaamsgsits@gmail.com';
-$subject = 'Message From '.$name.' on aayaam.sgsits.ac.in';
-$header='From:aayaamsgsits@gmail.com' . "\r\n" .
-   'Reply-To: no-reply@aayaam.sgsits.ac.in' . "\r\n" .
-   'X-Mailer: PHP/' . phpversion();
-$message=$message."\n".$contact;
-$send=mail($to,$subject,$message,$header);
+if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])):
+        //your site secret key
+        $secret = '6LdUqxcUAAAAABu5at6UpmGbPSX1FBxhCmeWl1QZ';
+        //get verify response data
+        $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+        $responseData = json_decode($verifyResponse);
+        if($responseData->success):
+            echo "its ok";
+            $to = 'aayaamsgsits@gmail.com';
+            $subject = 'Message From '.$name.' on aayaam.sgsits.ac.in';
+            $header='From:aayaamsgsits@gmail.com' . "\r\n" .
+               'Reply-To: no-reply@aayaam.sgsits.ac.in' . "\r\n" .
+               'X-Mailer: PHP/' . phpversion();
+            $message=$message."\n".$contact;
+            $send=mail($to,$subject,$message,$header);
 
-if($send)
-	echo 'done';
-else
-	echo 'false';
+            if($send)
+                echo 'done';
+            else
+                echo 'false';
 
 ?>
